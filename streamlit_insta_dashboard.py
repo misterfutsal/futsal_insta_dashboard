8a505face40e80f4dfb875948625689ae4d3c57f
Mister Futsal <mister.futsal@gmx.de>
2026-03-10 20:32:38 +0100

    Update streamlit_insta_dashboard.py

--- streamlit_insta_dashboard.py
+++ streamlit_insta_dashboard.py
@@ -425,7 +425,7 @@ with tab_zuschauer:
         unique_seasons = sorted([s for s in df_z['SAISON'].unique() if s != "Unbekannt"])
         color_map = {s: ('#0047AB' if i % 2 == 0 else '#FFC000') for i, s in enumerate(unique_seasons)}
         
-        st.markdown(f":grey[Hinweis: Spieltag 19 = Relegation; 20 = Viertelfinale; 21 = Halbfinale ; 22 = Finale]")
+       
         
         if 'HEIM' in df_z.columns:
             options_list = ["🇩🇪 Liga-Gesamtentwicklung (Spieltag-Schnitt)"] + sorted(df_z['HEIM'].unique())
@@ -532,6 +532,7 @@ with tab_zuschauer:
                     
                     fig_trend.update_traces(textposition='outside')
                     st.plotly_chart(fig_trend, use_container_width=True)
+                    st.markdown(f":grey[Hinweis: Spieltag 19 = Relegation; 20 = Viertelfinale; 21 = Halbfinale ; 22 = Finale]")
                     
                 else:
                     st.warning("Die erforderlichen Spalten (SAISON, SPIELTAG, AVERAGE_SPIELTAG) fehlen im Datensatz.")
@@ -578,6 +579,7 @@ with tab_zuschauer:
                     st.plotly_chart(fig_team, use_container_width=True)
     else: 
         st.error("Zuschauer-Daten konnten nicht geladen werden.")
+
 
 
 
