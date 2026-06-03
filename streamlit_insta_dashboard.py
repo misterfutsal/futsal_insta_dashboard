f932de1fa2d9c57540a7807fa40baf99a0389f68
Mister Futsal <mister.futsal@gmx.de>
2026-06-03 21:25:17 +0200

    Update streamlit_insta_dashboard.py

--- streamlit_insta_dashboard.py
+++ streamlit_insta_dashboard.py
@@ -150,7 +150,6 @@ with tab_insta:
         # Entferne inaktive CLubs und speichere diese separat
         liste_ausschluss_inaktiv = [
             "Beton Boys ",
-            "TSV Neuried"
         ]
         df_excluded = df_latest_display[df_latest_display['CLUB_NAME'].isin(liste_ausschluss_inaktiv)].copy()
         df_excluded.insert(0, 'RANG', range(1, len(df_excluded) + 1))
